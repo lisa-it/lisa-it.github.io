8d92aa04c6acb49eaf2f272c3423101a4e1e3afd
Lisa-IT <lisa.ejob@gmail.com>
2020-09-20 12:43:48 +0200

    Changes in August.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-function calcAmount () {
+function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let amount = parseInt(amountInput.value) * price;
-    alert("Fizetendő: " + amount + " Ft");
+    let showAmount = document.querySelector("span.show-amount");
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    
+    if (amountNumber > 10) {
+        alert("Maximum 10 termék!");
+    } else if (amountNumber < 1) {
+        alert("Minimum 1 termék!");
+    } else {
+        let amount = amountNumber * price;
+        showAmount.innerHTML = amount;
+    }
 }
