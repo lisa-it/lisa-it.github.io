b54cd6ee2a52fd66a9e47a1dee0114e826df66b3
unknown <lisa.ejob@gmail.com>
2022-05-30 11:06:19 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function calcAmount() {
-    let price = 1000;
+    let price = 10;
     let amountInput = document.querySelector("input[name='amount-input']");
     let showAmount = document.querySelector("span.show-amount");
     let amountNumber = parseInt(amountInput.value);
